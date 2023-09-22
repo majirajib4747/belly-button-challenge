1dc9b87c3b6028dc534eda51366d3f72d121130c
majirajib4747 <43809492+majirajib4747@users.noreply.github.com>
2023-09-22 10:47:08 -0400

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -15,115 +15,10 @@ function init() {
         var first_id = data.names[0]
         console.log(first_id);
         buildMetadata(first_id)
-
-        var sample_values = data.samples.map(x=> x.sample_values);
-        var otu_ids = data.samples.map(x=> x.otu_ids);
-        var otu_label = data.samples.map(x=> x.otu_labels);
-
-        var sorted_test = sample_values.sort(function(a, b){return b-a});
-        var top_ten = sorted_test.map(x => x.slice(0,10));
-        var sorted_ids = otu_ids.sort(function(a, b){return b-a});
-        var top_ids = sorted_ids.map(x =>x.slice(0,10));
-        var sorted_labels = otu_label.sort(function(a, b){return b-a});
-        var top_labels = sorted_labels.map(x =>x.slice(0,10));
-        // Data for Bar Chart
-        var trace1 = {
-            x : top_ten[0],
-            y : top_ids[0].map(x => "OTU " + x),
-            text : top_labels[0],
-            type : 'bar',
-            orientation : 'h',
-            transforms: [{
-                type: 'sort',
-                target: 'y',
-                order: 'descending',
-              }],
-            marker: {
-              color: 'blue',
-                
-              line: {
-                
-                width: 1.5
-              }
-            }
-        };
-        // layout for bar chart
-        var layout1 = {
-            title : '<b>Top 10 OTU</b>',
-            automargin: true,
-            autosize: true,
-            margin: {
-                l: 150,
-                r: 50,
-                b: 50,
-                t: 50,
-                pad: 4      
-    }
-        };
-      
-        // Draw the bar chart
-        var data = [trace1];
-        var config = {responsive:true}
-        Plotly.newPlot('bar', data, layout1,config);
-
-        // Data for bubble graph
-        var trace2 = {
-            x : otu_ids[0],
-            y : sample_values[0],
-            text : otu_label[0],
-            
-            mode : 'markers',
-            marker : {
-                color : otu_ids[0],
-                size : sample_values[0],
-                colorscale: 'YlGnBu'
-            }
-            
-        };
-      
-        // layout for bubble graph
-        var layout2 = {
-            title: '<b>Bubble Chart</b>',
-            automargin: true,
-            autosize: true,
-            showlegend: false,
-                margin: {
-                    l: 150,
-                    r: 50,
-                    b: 50,
-                    t: 50,
-                    pad: 4      
-        }};
-      
-        // Draw the bubble graph
-        var data2 = [trace2];
-        var config = {responsive:true}
-        Plotly.newPlot('bubble',data2,layout2,config);
-
-        
-                
-          
-
+        updateCharts(first_id);  
         });
     }; 
-    function buildMetadata(sample) {
-        d3.json("samples.json").then((data) => {
-          const metadata = data.metadata;
-          // Filter the data for the object with the desired sample number
-          const resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
-          const result = resultArray[0];
-          // Use d3 to select the panel with id of `#sample-metadata`
-          const PANEL = d3.select("#sample-metadata");
-      
-          // Use `.html("") to clear any existing metadata
-          PANEL.html("");
-      
-          
-          for (const [key, value] of Object.entries(result)) {
-            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-          }
-        });
-      }
+    
       function buildMetadata(sample) {
         d3.json("samples.json").then((data) => {
           const metadata = data.metadata;
@@ -142,9 +37,72 @@ function init() {
           }
         });
       }  
+
+      function updateCharts(sample) {    
+        d3.json("samples.json").then((data) => {
+          var samples = data.samples;
+          var filterArray = samples.filter(sampleObject => sampleObject.id == sample);
+          var result = filterArray[0];
+          var sample_values = result.sample_values;
+          var otu_ids = result.otu_ids;
+          var otu_labels = result.otu_labels;  
+
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: 'markers',
+            marker: {
+            size: sample_values,
+            color: otu_ids,
+            colorscale:"YlGnBu"
+            }
+        };
+        var data = [trace2];
+        var layout = {
+            title: '<b>Bubble Chart</b>',
+            automargin: true,
+            autosize: true,
+            showlegend: false,
+            margin: {l: 150,
+              r: 50,
+              b: 50,
+              t: 50,
+              pad: 4 }
+        };
+        Plotly.newPlot('bubble', data, layout); 
+        var trace1 = {
+          x: sample_values.slice(0,10).reverse(),
+          y: otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+          text: otu_labels.slice(0,10).reverse(),
+          name: "Greek",
+          type: "bar",
+          orientation: "h",marker: {
+            color: 'blue',
+              
+            line: {
+              
+              width: 1.5
+            }
+          }
+          
+      };
+      var data = [trace1];
+      var layout = {
+          title: "Top 10 OTU for sample " +sample,
+          automargin: true,
+          autosize: true,
+          margin: {l: 150, r: 50, t: 50, b: 50,pad: 4},
+          
+      };
+      var config = {responsive:true}
+      Plotly.newPlot("bar", data, layout); 
+        });
+      }
+
       function optionChanged(newSample) {
         // Fetch new data each time a new sample is selected
         buildMetadata(newSample);
-        
+        updateCharts(newSample)
       } 
     init();                   
